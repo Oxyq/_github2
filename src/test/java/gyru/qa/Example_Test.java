7b2c92cd896be31386a252f58b84a9cdea2a753b
IBM <seo-x@mail/ru>
2023-02-27 20:42:11 +0300

    add gitignore

--- src/test/java/gyru/qa/Example_Test.java
+++ src/test/java/gyru/qa/Example_Test.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Test;
 
 public class Example_Test {
     @Test
-    void example_test() {
+    void example_test0() {
         Assertions.assertTrue(2>1);
     }
 
     @Test
-    void example_test() {
+    void example_test0() {
         Assertions.assertTrue(3>2);
     }
 }
