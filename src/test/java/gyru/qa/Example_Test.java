04a88401ff1c38c41d9fee62d5c167ca31f4411f
IBM <seo-x@mail/ru>
2023-02-27 20:40:06 +0300

    add gitignore

---
+++ src/test/java/gyru/qa/Example_Test.java
@@ -0,0 +1,16 @@
+package gyru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Example_Test {
+    @Test
+    void example_test() {
+        Assertions.assertTrue(2>1);
+    }
+
+    @Test
+    void example_test() {
+        Assertions.assertTrue(3>2);
+    }
+}
