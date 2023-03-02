4ebdea282c4cdb06e0ef51cd7d7924a8608b09dc
IBM <seo-x@mail/ru>
2023-03-02 20:15:46 +0300

    use random

--- src/test/java/gyru/qa/Example_Test.java
+++ src/test/java/gyru/qa/Example_Test.java
@@ -3,14 +3,16 @@ package gyru.qa;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class Example_Test {
     @Test
     void example_test0() {
-        Assertions.assertTrue(2>1); //0000
+        Assertions.assertTrue(new Random().nextInt() > 1); //000
     }
 
     @Test
     void example_test1() {
-        Assertions.assertTrue(3>2); // 76767
+        Assertions.assertTrue(3>2); // 0O0
     }
 }
